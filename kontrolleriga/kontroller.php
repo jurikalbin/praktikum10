ce8c22db6295a244b9e942c67d071e6ab8c47eae
Jüri Kalbin <jkalbin@enos.itcollege.ee>
2017-04-23 22:15:07 +0300

    Homework 2

---
+++ kontrolleriga/kontroller.php
@@ -0,0 +1,42 @@
+<?php 
+	session_start();
+
+require_once('head.html'); 
+	$gallery=array(
+		array("pic"=>"pildid/nameless1.jpg","alt"=>"nimetu 1"),
+		array("pic"=>"pildid/nameless2.jpg","alt"=>"nimetu 2"),
+		array("pic"=>"pildid/nameless3.jpg","alt"=>"nimetu 3"),
+		array("pic"=>"pildid/nameless4.jpg","alt"=>"nimetu 4"),
+		array("pic"=>"pildid/nameless5.jpg","alt"=>"nimetu 5"),
+		array("pic"=>"pildid/nameless6.jpg","alt"=>"nimetu 6"),
+	);
+	
+	if (isset($_GET['page']) && $_GET['page']!="" ) {
+			$param=$_GET['page'];
+	} else {
+		$param="pealeht.html";
+	}
+		
+	switch($param){
+		case "pealeht":
+			include('pealeht.html');
+		break;
+		case "galerii":
+			include('galerii.html');
+		break;
+		case "vote":
+			if (isset($_SESSION['voted'])){
+				include('tulemus.html');			
+			}else{
+				include('vote.html');
+			}
+		break;
+		case "tulemus":
+			include('tulemus.html');
+		break;
+		case "tyhistus":
+			include('tyhistus.php');
+			include('pealeht.html');
+		break;
+	}	
+require_once('foot.html'); ?>
---
+++ kontrolleriga/tyhistus.php
@@ -0,0 +1,7 @@
+<?php
+	if(isset($_COOKIE[session_name()])) {
+		setcookie(session_name(), '', time()-42000, '/');
+		}
+	$_SESSION = array();
+		session_destroy();
+?>
